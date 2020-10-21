23559719cd7b44887f0a56bd2fa3ce178fca18d2
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-10-20 22:22:52 -0300

    update

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -20,8 +20,12 @@ export class NegociacaoController {
 
     adiciona(event: Event) {
         event.preventDefault();
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+        if (!this._ehDiaUtil(data)) {
+            this._mensagemView.update('Somente negociações em dias úteis, por favor');
+        }
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, ',')),
+            data,
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val())
         );
@@ -30,4 +34,18 @@ export class NegociacaoController {
         this._mensagemView.update('Negociacao adicionada com sucesso!');
     }
 
+    private _ehDiaUtil(data: Date) {
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
